1a58af8c7af93ccf18a3b7bd4525796986e54d89
Bence Ágocsi-Kiss <akbence007@gmail.com>
2019-01-21 00:48:33 +0100

    Backend works now

--- greenflow-backend/src/main/java/rest/RestService.java
+++ greenflow-backend/src/main/java/rest/RestService.java
@@ -1,13 +1,15 @@
+package rest;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-@Path("/hello")
-public class MessageRestService {
+@Path("")
+public class RestService {
 
     @GET
-    @Path("/")
+    @Path("/hello")
     public Response printMessage() {
-        public Response printMessage () {
             String result = "Hello Backend";
             return Response.status(200).entity(result).build();
         }
