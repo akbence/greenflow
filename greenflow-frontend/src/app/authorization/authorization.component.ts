0808c45e2ab8f4b3a76d79c2bbd9dd898f6fa055
Ágocsi-Kiss Bence <akbence007@gmail.com>
2019-02-06 01:00:28 +0100

    initial login/register ui component, not working

---
+++ greenflow-frontend/src/app/authorization/authorization.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-authorization',
+    templateUrl: './authorize.component.html',
+    styleUrls: ['./authorize.component.css']
+  })
+
+  export class AuthorizationComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {}
+  }
+
--- greenflow-frontend/src/app/layouts/admin-layout/admin-layout.module.ts
+++ greenflow-frontend/src/app/layouts/admin-layout/admin-layout.module.ts
@@ -16,6 +16,7 @@ import { IconsComponent } from '../../icons/icons.component';
 import { MapsComponent } from '../../maps/maps.component';
 import { NotificationsComponent } from '../../notifications/notifications.component';
 import { UpgradeComponent } from '../../upgrade/upgrade.component';
+import {AuthorizationComponent} from '../../authorization/authorization.component'
 
 
 @NgModule({
@@ -34,7 +35,8 @@ import { UpgradeComponent } from '../../upgrade/upgrade.component';
     IconsComponent,
     MapsComponent,
     NotificationsComponent,
-    UpgradeComponent
+    UpgradeComponent,
+    AuthorizationComponent
   ]
 })
 
