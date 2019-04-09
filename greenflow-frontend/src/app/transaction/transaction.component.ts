9fac77a8027e344a7b0253765ba6222b104cf78a
akbence <akbence007@gmail.com>
2019-04-09 13:32:46 +0200

    new transaction ui fixed

--- greenflow-frontend/src/app/transaction/transaction.component.ts
+++ greenflow-frontend/src/app/transaction/transaction.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { tokenKey } from '@angular/core/src/view';
 import { Transaction } from './model/transaction';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-transaction',
@@ -9,22 +10,26 @@ import { Transaction } from './model/transaction';
 })
 export class TransactionComponent implements OnInit {
 
-  
+  public transaction : Transaction  
 
-  public transaction : Transaction
-  
+  date = new FormControl(new Date());
+  serializedDate = new FormControl((new Date()).toISOString());
 
   constructor() {
       this.transaction = new Transaction()
    }
 
+
   ngOnInit() {
+    document.getElementById('transactionDate').nodeValue = new Date().toDateString()
+    
     this.transaction.name = "New Item"
     this.transaction.ammount = 0
     this.transaction.currency = "HUF"
     this.transaction.category = null
     this.transaction.paymentType = "CASH"
-    this.transaction.transactionDate = new Date()
+    this.transaction.transactionDate = <any> new Date().toLocaleDateString()
+    console.log( this.transaction.transactionDate)
   }
 
 }
