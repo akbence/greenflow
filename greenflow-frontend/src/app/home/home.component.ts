f77a90a123d20313376c5ced60d85e976044ca33
Ágocsi-Kiss Bence <akbence007@gmail.com>
2019-04-21 02:41:50 +0200

    Month picker added to statistics in frontend

--- greenflow-frontend/src/app/home/home.component.ts
+++ greenflow-frontend/src/app/home/home.component.ts
@@ -2,11 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
 import { LegendItem, ChartType } from '../lbd/lbd-chart/lbd-chart.component';
 import * as Chartist from 'chartist';
+import {FormControl} from '@angular/forms';
+import {MatDatepicker} from '@angular/material/datepicker';
+import * as _moment from 'moment';
+// tslint:disable-next-line:no-duplicate-imports
+import { Moment} from 'moment'
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+import {MomentDateAdapter} from '@angular/material-moment-adapter';
+
+const moment = _moment;
+
+export const MY_FORMATS = {
+  parse: {
+    dateInput: 'MM/YYYY',
+  },
+  display: {
+    dateInput: 'MM/YYYY',
+    monthYearLabel: 'MMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+               {provide: MAT_DATE_FORMATS, useValue: MY_FORMATS}]
 })
 export class HomeComponent implements OnInit {
 
@@ -24,4 +47,19 @@ export class HomeComponent implements OnInit {
 
     } 
 
+    date = new FormControl(moment());
+
+    chosenYearHandler(normalizedYear: Moment) {
+      const ctrlValue = this.date.value;
+      ctrlValue.year(normalizedYear.year());
+      this.date.setValue(ctrlValue);
+    }
+  
+    chosenMonthHandler(normalizedMonth: Moment, datepicker: MatDatepicker<Moment>) {
+      const ctrlValue = this.date.value;
+      ctrlValue.month(normalizedMonth.month());
+      this.date.setValue(ctrlValue);
+      datepicker.close();
+      console.log(ctrlValue.month())
+    }
 }
