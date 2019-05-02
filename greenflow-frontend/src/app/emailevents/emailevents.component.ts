285847c94d37bd7bc720b0987d96116f317b6133
Ágocsi-Kiss Bence <akbence007@gmail.com>
2019-05-02 02:34:46 +0200

    events for frontend design done

--- greenflow-frontend/src/app/emailevents/emailevents.component.ts
+++ greenflow-frontend/src/app/emailevents/emailevents.component.ts
@@ -11,56 +11,25 @@ import { HttpHeaders } from '@angular/common/http';
 })
 export class EmaileventsComponent implements OnInit {
 
-  private username : string
-  private registrationDate : Date
-  private isEmailEditing : boolean
-  private email : string
-  private serverURL : string
+  private monthly : boolean
+
 
   constructor(private http: HttpClient,globals : Globals ) {
-    this.serverURL = globals.getBaseUrl()
+  
   }
 
 
   ngOnInit() {
-    this.username=JSON.parse(localStorage.getItem("currentUser")).username
-    this.registrationDate=JSON.parse(localStorage.getItem("currentUser")).registrationDate
-    this.email = JSON.parse(localStorage.getItem("currentUser")).email
-    this.isEmailEditing = false
-  }
-
-  emailUpdate(){
-    var token=  JSON.parse(localStorage.getItem("currentUser")).token
-    console.log(this.email)
-    const headers = new HttpHeaders()
-          .set("Authorization",token)
-          .append('Content-Type', 'application/json')
-    var storage = JSON.parse(localStorage.getItem('currentUser'))
-    if(this.email.length == 0){
-      this.emailRemove()
-      return
-    }
-    return this.http.request(storage.email ? 'put' : 'post', this.serverURL+"email",{ body: {email : this.email}, headers, observe : 'response',withCredentials : true})
-      .subscribe((res : any)=>{
-        
-        storage.email=this.email
-        localStorage.setItem('currentUser',JSON.stringify(storage))
-});
-  }
-
-  emailRemove(){
-    var token=  JSON.parse(localStorage.getItem("currentUser")).token
-    console.log(this.email)
-    const headers = new HttpHeaders()
-          .set("Authorization",token)
-          .append('Content-Type', 'application/json')
-    var storage = JSON.parse(localStorage.getItem('currentUser'))
-    return this.http.delete( this.serverURL+"email",{  headers, observe : 'response',withCredentials : true})
-      .subscribe((res : any)=>{
-        this.email=""
-        storage.email=null
-        localStorage.setItem('currentUser',JSON.stringify(storage))
-});
+    this.monthly = false
+    this.monthly = false
 
   }
+
+  test(){
+    console.log(this.monthly)
+  }
+  
+
+ 
+  
 }
