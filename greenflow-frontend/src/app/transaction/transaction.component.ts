4ddfea033d12ada8c24e2596666d6ac459aa10b5
Ágocsi-Kiss Bence <akbence007@gmail.com>
2019-05-12 01:59:51 +0200

    Added imagepicker frontend for transaction receipt OCR

--- greenflow-frontend/src/app/transaction/transaction.component.ts
+++ greenflow-frontend/src/app/transaction/transaction.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { tokenKey } from '@angular/core/src/view';
 import { Transaction } from './model/transaction';
-import {FormControl} from '@angular/forms';
-import {Globals} from '../globals';
-import {HttpClient, HttpHeaders} from '@angular/common/http'
+import { FormControl } from '@angular/forms';
+import { Globals } from '../globals';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Resolve } from '@angular/router';
 import { formatDate } from '@angular/common';
 
@@ -19,24 +19,24 @@ export class TransactionComponent implements OnInit, Resolve<any> {
     this.queryAllCategories()
   }
 
-  public transaction : Transaction
-  public date : FormControl
-  public paymentTypes= ['CASH','CARD']
-  public serverURL : string
-  public categoryList : string []
+  public transaction: Transaction
+  public date: FormControl
+  public paymentTypes = ['CASH', 'CARD']
+  public serverURL: string
+  public categoryList: string[]
 
-  constructor(private http: HttpClient, globals : Globals) {
-      this.serverURL = globals.getBaseUrl()
-      this.transaction = new Transaction()
-      this.transaction.date=null
-      this.date=new FormControl(new Date())
-      this.categoryList = []
-      this.queryAllCategories()
-   }
+  constructor(private http: HttpClient, globals: Globals) {
+    this.serverURL = globals.getBaseUrl()
+    this.transaction = new Transaction()
+    this.transaction.date = null
+    this.date = new FormControl(new Date())
+    this.categoryList = []
+    this.queryAllCategories()
+  }
 
 
 
-  ngOnInit() {    
+  ngOnInit() {
     this.transaction.name = "New Item"
     this.transaction.amount = 0
     this.transaction.currency = "HUF"
@@ -44,65 +44,110 @@ export class TransactionComponent implements OnInit, Resolve<any> {
     console.log(this.categoryList[0])
   }
 
-  addExpense(){
+  addExpense() {
     this.transaction.isExpense = true
     this.addTransaction()
   }
 
-  addIncome(){
+  addIncome() {
     this.transaction.isExpense = false
     this.addTransaction()
   }
 
-  addTransaction(){
-    
-    this.transaction.date = this.formatDate((<Date> this.date.value)).toString() 
-    const req=JSON.stringify(this.transaction)
-    var token=  JSON.parse(localStorage.getItem("currentUser")).token
-    const headers = new HttpHeaders()
-            .set("Authorization",token)
-            .append('Content-Type', 'application/json')
-    return this.http.request<any>('post',this.serverURL+"postTransaction",{headers,body : req, observe : 'response',withCredentials : true})
-    .subscribe (
-      (data : any) =>{
-      },
-      error => {
-          //this.error = error;
-          console.log("Error"+error);
-      });
+  addTransaction() {
 
-    }
-
-  queryAllCategories(){
-    var token=  JSON.parse(localStorage.getItem("currentUser")).token
+    this.transaction.date = this.formatDate((<Date>this.date.value)).toString()
+    const req = JSON.stringify(this.transaction)
+    var token = JSON.parse(localStorage.getItem("currentUser")).token
     const headers = new HttpHeaders()
-            .set("Authorization",token);
-    return this.http.get<any>(this.serverURL+"categories",{headers,withCredentials : true})
-    .pipe()
-    .subscribe(
-      data =>{
-        data.forEach(element => {
-          this.categoryList.push(element.name)
-          
-        })
-        this.transaction.category=this.categoryList[0]
-      },
-      error => {
+      .set("Authorization", token)
+      .append('Content-Type', 'application/json')
+    return this.http.request<any>('post', this.serverURL + "postTransaction", { headers, body: req, observe: 'response', withCredentials: true })
+      .subscribe(
+        (data: any) => {
+        },
+        error => {
           //this.error = error;
-          console.log("Error"+error);
-      });
+          console.log("Error" + error);
+        });
+
+  }
+
+  queryAllCategories() {
+    var token = JSON.parse(localStorage.getItem("currentUser")).token
+    const headers = new HttpHeaders()
+      .set("Authorization", token);
+    return this.http.get<any>(this.serverURL + "categories", { headers, withCredentials: true })
+      .pipe()
+      .subscribe(
+        data => {
+          data.forEach(element => {
+            this.categoryList.push(element.name)
+
+          })
+          this.transaction.category = this.categoryList[0]
+        },
+        error => {
+          //this.error = error;
+          console.log("Error" + error);
+        });
   }
 
   private formatDate(date) {
     var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
 
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
 
     return [year, month, day].join('-');
-}
+  }
 
+  selectedFile: File
+  base64 : string
+
+  onFileChanged(event) {
+    this.selectedFile = event.target.files[0]
+    var reader = new FileReader();
+    /* reader.readAsDataURL(this.selectedFile);
+    var _this = this;
+    reader.onload = function () {
+      _this.base64 = btoa(<any>reader.result);
+      console.log("done") */
+
+    
+        reader.onload =this._handleReaderLoaded.bind(this);
+
+        reader.readAsBinaryString(event.target.files[0]);
+   
+  }
+
+  onUpload() {
+    var token = JSON.parse(localStorage.getItem("currentUser")).token
+    const headers = new HttpHeaders()
+      .set("Authorization", token)
+      .append("Content-Type","application/octet-stream")
+    return this.http.request<any>("post", this.serverURL + "test", { body: this.base64, headers, withCredentials: true })
+      .pipe()
+      .subscribe(
+        data => {
+          data.forEach(element => {
+            this.categoryList.push(element.name)
+
+          })
+          this.transaction.category = this.categoryList[0]
+        },
+        error => {
+          //this.error = error;
+          console.log("Error" + error);
+        });
+  }
+
+  _handleReaderLoaded(readerEvt) {
+    var binaryString = readerEvt.target.result;
+           this.base64= btoa(binaryString);
+           console.log(btoa(binaryString));
+   }
 }
